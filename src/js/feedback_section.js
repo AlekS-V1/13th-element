075cdf1ebe6853cd0d5905ce92497b2c770ea30d
Alex <vlipoq@gmail.com>
2025-08-09 22:47:12 +0200

    Update path stars feedback_section.js

--- src/js/feedback_section.js
+++ src/js/feedback_section.js
@@ -77,13 +77,13 @@ function createStarRating(rate) {
           () => `
           <div class="star">
             <svg class="star-empty">
-              <use href="./img/sprite.svg#star-empty"></use>
+              <use href="#star-empty"></use>
             </svg>
             <svg class="star-half">
-              <use href="./img/sprite.svg#star-half"></use>
+              <use href="#star-half"></use>
             </svg>
             <svg class="star-filled">
-              <use href="./img/sprite.svg#star-filled"></use>
+              <use href="#star-filled"></use>
             </svg>
           </div>
         `
